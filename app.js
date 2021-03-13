137276a6f79bcbea9bfa145144d063a5cf3ce43e
Garry <gurjyotsingh76@gmail.com>
2021-03-13 05:48:33 +0530

    Adding the JavaScript

---
+++ app.js
@@ -0,0 +1,14 @@
+let x = 5;
+let y = 'Hello';
+let z = true;
+const Constant = 20;
+
+if (x === y) {
+    console.log("they match")
+  } else {
+    console.log("no luck")
+}
+
+for (i=0;i<y.length; i++) {
+    console.log(y.charAt(i))
+}
